d8e7769491f224d174579220353c1eaf997266fa
pablolagreca <Neruda10>
2017-08-29 14:19:42 -0300

    pr feedback

--- src/test/java/org/mule/service/scheduler/internal/ThrottledSchedulerThrottleTestCase.java
+++ src/test/java/org/mule/service/scheduler/internal/ThrottledSchedulerThrottleTestCase.java
@@ -8,11 +8,12 @@ package org.mule.service.scheduler.internal;
 
 import static java.lang.Thread.sleep;
 import static java.util.concurrent.Executors.newSingleThreadExecutor;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 import static org.mule.service.scheduler.ThreadType.CUSTOM;
-
 import org.mule.runtime.core.api.util.concurrent.Latch;
 import org.mule.tck.probe.JUnitLambdaProbe;
 import org.mule.tck.probe.PollingProber;
@@ -21,18 +22,18 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.quartz.SchedulerException;
 
-import io.qameta.allure.Description;
-import io.qameta.allure.Feature;
-
 @Feature("Scheduler Throttling")
 public class ThrottledSchedulerThrottleTestCase extends BaseDefaultSchedulerTestCase {
 
   private static final int THROTTLE_SIZE = 2;
+  private static final int SINGLE_TASK_THROTTLE_SIZE = 1;
   private ExecutorService outerExecutor;
 
   @Override
@@ -51,9 +52,20 @@ public class ThrottledSchedulerThrottleTestCase extends BaseDefaultSchedulerTest
   }
 
   @Test
+  @Description("Tests that a 'maxConcurrentTasks=1' configuration allows to execute a single task")
+  public void oneConcurrentTaskSupported() throws InterruptedException {
+    final ScheduledExecutorService scheduler = createExecutor(SINGLE_TASK_THROTTLE_SIZE);
+    Latch latch = new Latch();
+    scheduler.submit(() -> latch.countDown());
+    if (!latch.await(200, MILLISECONDS)) {
+      fail("Task never executed");
+    }
+  }
+
+  @Test
   @Description("Tests that a task submitted in excess of 'maxConcurrentTasks' waits until another task finishes before executing.")
   public void throttledTask() throws InterruptedException {
-    final ScheduledExecutorService scheduler = createExecutor();
+    final ScheduledExecutorService scheduler = createExecutor(THROTTLE_SIZE);
     final Latch latch = new Latch();
 
     for (int i = 0; i < THROTTLE_SIZE; ++i) {
@@ -79,10 +91,9 @@ public class ThrottledSchedulerThrottleTestCase extends BaseDefaultSchedulerTest
     }));
   }
 
-  @Override
-  protected ScheduledExecutorService createExecutor() {
+  protected ScheduledExecutorService createExecutor(int throttleSize) {
     return new ThrottledScheduler(BaseDefaultSchedulerTestCase.class.getSimpleName(), sharedExecutor, 1, sharedScheduledExecutor,
-                                  sharedQuartzScheduler, CUSTOM, THROTTLE_SIZE, () -> 5000L, EMPTY_SHUTDOWN_CALLBACK);
+                                  sharedQuartzScheduler, CUSTOM, throttleSize, () -> 5000L, EMPTY_SHUTDOWN_CALLBACK);
   }
 
 }
