2b21b02ae2bbf38662ebe91d0678c60c7b20af38
Hyeran Park <53480609+hyerans@users.noreply.github.com>
2025-05-15 14:24:19 -0700

    W-18347237: Avoid scheduling tasks with infinite/zero timeout (#319) (#321)

---
+++ src/main/java/org/mule/service/scheduler/internal/AbstractDelegatedScheduledFuture.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2023 Salesforce, Inc. All rights reserved.
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.service.scheduler.internal;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.RunnableFuture;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Abstract base class for {@link ScheduledFuture} implementations that delegate most operations to an underlying
+ * {@link RunnableFuture}.
+ *
+ * @since 1.10
+ */
+abstract class AbstractDelegatedScheduledFuture<V> implements ScheduledFuture<V> {
+
+  protected final RunnableFuture<V> task;
+
+  /**
+   * Creates a new instance that delegates to the given task.
+   *
+   * @param task the task to delegate to
+   */
+  protected AbstractDelegatedScheduledFuture(RunnableFuture<V> task) {
+    this.task = task;
+  }
+
+  @Override
+  public boolean cancel(boolean mayInterruptIfRunning) {
+    return task.cancel(mayInterruptIfRunning);
+  }
+
+  @Override
+  public boolean isCancelled() {
+    return FutureUtils.isCancelled(task);
+  }
+
+  @Override
+  public boolean isDone() {
+    return FutureUtils.isDone(task);
+  }
+
+  @Override
+  public V get() throws InterruptedException, ExecutionException {
+    return FutureUtils.get(task);
+  }
+
+  @Override
+  public V get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+    return FutureUtils.get(task, timeout, unit);
+  }
+}
---
+++ src/main/java/org/mule/service/scheduler/internal/ZeroDelayScheduledFuture.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2023 Salesforce, Inc. All rights reserved.
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.service.scheduler.internal;
+
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
+
+import java.util.concurrent.Delayed;
+import java.util.concurrent.RunnableFuture;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Implementation of {@link ScheduledFuture} for tasks that have no pending delay. This can represent tasks that are intended for
+ * immediate execution or tasks that have already been cancelled prior to scheduling. All operations are delegated to the
+ * underlying {@link RunnableFuture}.
+ *
+ * @since 1.10
+ */
+final class ZeroDelayScheduledFuture<V> extends AbstractDelegatedScheduledFuture<V> {
+
+  /**
+   * Creates a new instance that delegates to the given task.
+   *
+   * @param task the task to delegate to
+   */
+  ZeroDelayScheduledFuture(RunnableFuture<V> task) {
+    super(task);
+  }
+
+  @Override
+  public long getDelay(TimeUnit unit) {
+    return 0;
+  }
+
+  @Override
+  public int compareTo(Delayed o) {
+    long diff = getDelay(NANOSECONDS) - o.getDelay(NANOSECONDS);
+    return Long.compare(diff, 0);
+  }
+}
